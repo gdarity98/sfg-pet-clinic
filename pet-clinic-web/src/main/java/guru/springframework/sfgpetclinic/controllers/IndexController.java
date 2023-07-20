3326cef1a029f24d553cce6aca5c3d8cce56a644
Gabe Darity <38188352+gdarity98@users.noreply.github.com>
2023-07-20 15:56:04 -0600

    Create Index Page and Controller. Closes #8.

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by gd on 7/20/2023
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
+}
