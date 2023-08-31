bcb8dcaaa173b81877e763707e5c3a4d1471290f
Gabe Darity <38188352+gdarity98@users.noreply.github.com>
2023-08-31 15:42:23 -0600

    Create Pet Type Map Service. Closes #29.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created by gd on 8/31/2023
+ */
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
